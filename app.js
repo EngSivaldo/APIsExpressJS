f52d2140637bd67fb2c092469816e7bc7d783209
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-15 09:33:30 -0300

    Recuperando as informações de um piloto a partir do id (GET- -drivers--id)

--- app.js
+++ app.js
@@ -14,12 +14,20 @@ app.get(baseAPIRoute + '/drivers', (req, resp) => {
   resp.status(200).send(drivers);//entregar a lista
 })
 
-//estabelecer nova rota
+//rota que recupera infor piloto especifico pela posicao
 app.get(baseAPIRoute + '/drivers/standings/:position', (req, resp) => {
-  const position = req.params.position;
+  const {position} = req.params;
   const selectDrivers = drivers[position-1]
   resp.status(200).send(selectDrivers);//entregar a lista
 });
+
+//rota do (id)
+app.get(baseAPIRoute + '/drivers/:id', (req, res) => {
+  const {id} = req.params;
+  const selectDrivers = drivers.find(driver => driver.id === id);
+  res.status(200).send(selectDrivers)
+
+})
 
 //variavel guarda a porta
 const port = 3000;
